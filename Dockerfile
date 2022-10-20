67a7b9f0faec9888da72a1b0810dedfbf472eb7e
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-10-20 21:13:18 +0530

    Added express server

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /app
 COPY ./package.json .
 RUN npm install
 COPY . .
-ENV APP_PORT 4000
+ENV APP_PORT 3000
 CMD npm run start
-EXPOSE 4000
+EXPOSE 3000
--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hello Rishikant");
+// PACKAGE LIBS
+const express = require('express');
+
+// Creating new express app
+const app = express();
+
+// Getting the port to listen on
+const port = process.env.APP_PORT;
+
+// Dummy endpoint
+app.get('/', (req, res) => {
+    res.send(200);
+});
+
+// Listening on the given port
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
