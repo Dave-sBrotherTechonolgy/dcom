b61eae7d2e189e79ca10e7bedc1656ff861eaf3a
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-10-31 13:44:25 +0530

    Added sample data

---
+++ data/sample/developer.js
@@ -0,0 +1,54 @@
+{
+    "id": "1",
+    "summary": {
+        "name": "first_name_1 last_name_1",
+        "description" : "profile1_description",
+        "urls": [
+            "url_1"
+        ],
+        "followers": 0,
+        "connections": 0,
+        "profile_picture": "profile_1_picture_url",
+        "profile_banner": "profile_1_banner_url",
+        "about": "profile_1_about"
+    },
+    "featured": [
+        {
+            "title": "featured_1_title",
+            "url": "featured_1_url",
+            "description": "featured_1_description"
+        }
+    ],
+    "education": [
+        {
+            "institution_name": "institution_1_name",
+            "qualificaiton": "institution_1_qualification"
+        }
+    ],
+    "certifications": [
+        {
+            "certification_name": "certificate_1_name",
+            "provider": "certificate_1_provider",
+            "issue_date": "certificate_1_issue_date",
+            "expiry_date": "certificate_1_expiry_date"
+        }
+    ],
+    "skills": [
+        "skill1"
+    ],
+    "projects": [
+        {
+            "name": "project_1_name",
+            "start_date": "project_1_start_date",
+            "end_date": "project_1_end_date",
+            "description": "project_1_description",
+            "url": "project_1_url"
+        }
+    ],
+    "languages": [
+        {
+            "name": "language_1",
+            "proficiency": "proficiency_1"
+        }
+    ]
+}
