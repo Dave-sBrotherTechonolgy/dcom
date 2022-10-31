fb88ccbe99ca6bdf8a92278ccde611d92825de30
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-10-31 13:41:50 +0530

    Added endpoints for fetching various data related to developer profile

--- handlers/Profile.js
+++ handlers/Profile.js
@@ -4,17 +4,107 @@
 const { client, dbConfig, connectDB } = require('../data/connector');
 
 /**
- * @returns The profile data of the user with the given username
- * @param {*} id The id of the user whose profile data is to fetched
+ * @returns The profile data of the developer profile with the given username
+ * @param {*} id The id of the developer whose profile data is to fetched
  */
-const getUserProfile  = async (id) => {
+const getDevProfile  = async (id) => {
     // Connecting to database
     await connectDB();
 
     // Fetching data
-    const data = await client.db(dbConfig.dbName).collection(dbConfig.collections.Profiles).findOne({ id: id });
+    const data = await client.db(dbConfig.dbName).collection(dbConfig.collections.Profiles).findOne({ id: id }).then(data => data.summary);
 
     return data;
 };
 
-module.exports.getUserProfile = getUserProfile;
+/**
+ * @returns The list of featured of the developer profile with the given id
+ * @param {*} id 
+ */
+const getDevFeatured = async (id) => {
+    // Connecting to database
+    await connectDB();
+
+    // Fetching data
+    const data = await client.db(dbConfig.dbName).collection(dbConfig.collections.Profiles).findOne({ id: id }).then(data => data.featured);
+
+    return data;
+}
+
+/**
+ * @returns The list of education of the developer profile with the given id
+ * @param {*} id 
+ */
+const getDevEducation = async (id) => {
+    // Connecting to database
+    await connectDB();
+
+    // Fetching data
+    const data = await client.db(dbConfig.dbName).collection(dbConfig.collections.Profiles).findOne({ id: id }).then(data => data.education);
+
+    return data;
+}
+
+/**
+ * @returns The list of certifications of the developer profile with the given id
+ * @param {*} id 
+ */
+const getDevCertications = async (id) => {
+    // Connecting to database
+    await connectDB();
+
+    // Fetching data
+    const data = await client.db(dbConfig.dbName).collection(dbConfig.collections.Profiles).findOne({ id: id }).then(data => data.certifications);
+
+    return data;
+}
+
+/**
+ * @returns The list of skills of the developer profile with the given id
+ * @param {*} id 
+ */
+const getDevSkills = async (id) => {
+    // Connecting to database
+    await connectDB();
+
+    // Fetching data
+    const data = await client.db(dbConfig.dbName).collection(dbConfig.collections.Profiles).findOne({ id: id }).then(data => data.skills);
+
+    return data;
+}
+
+/**
+ * @returns The list of projects of the developer profile with the given id
+ * @param {*} id 
+ */
+const getDevProjects = async (id) => {
+    // Connecting to database
+    await connectDB();
+
+    // Fetching data
+    const data = await client.db(dbConfig.dbName).collection(dbConfig.collections.Profiles).findOne({ id: id }).then(data => data.projects);
+
+    return data;
+}
+
+/**
+ * @returns The list of languages of the developer profile with the given id
+ * @param {*} id 
+ */
+const getDevLanguages = async (id) => {
+    // Connecting to database
+    await connectDB();
+
+    // Fetching data
+    const data = await client.db(dbConfig.dbName).collection(dbConfig.collections.Profiles).findOne({ id: id }).then(data => data.languages);
+
+    return data;
+}
+
+module.exports.getDevProfile = getDevProfile;
+module.exports.getDevFeatured = getDevFeatured;
+module.exports.getDevEducation = getDevEducation;
+module.exports.getDevCertications = getDevCertications;
+module.exports.getDevSkills = getDevSkills;
+module.exports.getDevProjects = getDevProjects;
+module.exports.getDevLanguages = getDevLanguages;
--- routers/Profile.js
+++ routers/Profile.js
@@ -11,8 +11,39 @@ const ProfileRouter = express.Router();
 
 // Adding router endpoints
 
+// Endpoint to fetch all developer profile data
 ProfileRouter.get('/:id', async (req, res) => {
-    res.send(await profileHandlers.getUserProfile(req.params.id));
+    res.send(await profileHandlers.getDevProfile(req.params.id));
+});
+
+// Endpoint to fetch the list of featured of the given developer profile
+ProfileRouter.get('/:id/featured', async (req, res) => {
+    res.send(await profileHandlers.getDevFeatured(req.params.id));
+});
+
+// Endpoint to fetch the list of education of the given developer profile
+ProfileRouter.get('/:id/education', async (req, res) => {
+    res.send(await profileHandlers.getDevEducation(req.params.id));
+});
+
+// Endpoint to fetch the list of certifications of the given developer profile
+ProfileRouter.get('/:id/certifications', async (req, res) => {
+    res.send(await profileHandlers.getDevCertications(req.params.id));
+});
+
+// Endpoint to fetch the list of skills of the given developer profile
+ProfileRouter.get('/:id/skills', async (req, res) => {
+    res.send(await profileHandlers.getDevSkills(req.params.id));
+});
+
+// Endpoint to fetch the list of projects of the given developer profile
+ProfileRouter.get('/:id/projects', async (req, res) => {
+    res.send(await profileHandlers.getDevProjects(req.params.id));
+});
+
+// Endpoint to fetch the list of languages of the given developer profile
+ProfileRouter.get('/:id/languages', async (req, res) => {
+    res.send(await profileHandlers.getDevLanguages(req.params.id));
 });
 
 module.exports.ProfileRouter = ProfileRouter;
